9d1fff28e66a957987598ade55be2fb0ae7f9c78
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 01:47:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  // return? array where each line is [[musician] plays [instruments]].
+  var arrayInternal = [];
+  var stringInternal = "";
+
+  for (var i=0; i<musicians.length; i++) { // FOR LOOP INTERNALS SHOULD BE COMMAS!!!!
+    stringInternal = musicians[i] + " plays " + instruments[i];
+    arrayInternal[i] = stringInternal;
+  }
+
+  return arrayInternal;
+}
+
+function johnLennonFacts(array){
+  var arrayInternal = [];
+  var i = 0;
+
+  while (i < array.length){
+    arrayInternal[i] = array[i] + "!!!";
+    i++;
+  }
+
+  return arrayInternal;
+}
+
+function iLoveTheBeatles(number){
+  var arrayInternal = [];
+  var numberInt = number;
+  var i = 0;
+
+  do {
+    arrayInternal[i] = "I love the Beatles!";
+    i++;
+    numberInt++;
+  } while ( 15 - numberInt > 0 )
+
+  return arrayInternal;
+}
