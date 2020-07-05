4c6a8c631b3771fe5e3a4b11f7bb3f975c9b1061
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 01:27:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function theBeatlesPlay(musicians,instruments){
   // return? array where each line is [[musician] plays [instruments]].
   var arrayInternal = [];
-  var forCounter = musicians.length - 1;
-
+  //var forCounter = musicians.length - 1;
+  console.log(musicians.length - 1);
 }
 
 function johnLennonFacts(array){
