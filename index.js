9e504ce27dedb73e31b2607d34aeaf1333fdb741
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-05 01:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function theBeatlesPlay(musicians,instruments){
   // return? array where each line is [[musician] plays [instruments]].
   var arrayInternal = [];
   var stringInternal;
-  for (i=0, i<musicians.length, i++){
-    stringInternal = musicians[i] + "plays" + instruments[i];
-    arrayInternal[i] = stringInternal;
+  for (i=0, i<musicians.length, i++) {
+    //stringInternal = musicians[i] + "plays" + instruments[i];
+    //arrayInternal[i] = stringInternal;
   }
   return arrayInternal;
 }
